bdaee5cb89a76071fe0dd50beee88631048851c6
Stand <99733463+StandVideo@users.noreply.github.com>
2026-01-31 20:27:56 +0100

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- files/js/menu.js
+++ files/js/menu.js
@@ -84,7 +84,7 @@ var menuData = {
         "FW": ["9.xx","8.xx","7.xx"]
       },
       {
-        "PL-Title": "sign np save PS4",
+        "PL-Title": "Sign NP Save & Remote PS4",
         "PL-Bin": "pl_np_fake_signin_ps4.bin",
         "PL-Ver": "v1.00",
         "PL-Desc": "Activate Save & Remote PS4.",
@@ -976,6 +976,7 @@ var menuData = {
   }
 
 }
+
 
 
 
